16e3b5c3f6fc162d6fb38363a7968696c855d21a
Abdirahman <rizetracker@gmail.com>
2022-01-10 04:01:43 +0100

    flask api + joblib setup

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, request , json
+import joblib
+import os
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+
+app = Flask(__name__)
+
+def checklabel(label):
+    mapping = {
+        0: "Slecht",
+        1: "Gemiddeld",
+        2: "Goed"
+    }
+    return mapping[label]
+
+@app.route('/knn', methods=['POST'])
+def predict_knn():
+    # first load the model
+    dirname = os.getcwd()
+    filename_knn = os.path.join(dirname, 'models/knn')
+    knn = joblib.load(filename_knn)
+    # read the request data from the post request
+    response = request.get_json()
+    data = json.loads(response["data"])
+    # convert the data into a numpy array
+    arr = np.array(data)
+    # scale the data
+    scaler = StandardScaler()
+    arr = scaler.fit_transform(arr)
+    # predict the response
+    predictions = knn.predict(arr)
+    labels = map(checklabel, predictions.tolist())
+    labels = list(labels)
+    return json.dumps(labels)
+
+@app.route('/random-forest', methods=['POST'])
+def predict_random_forest():
+     # first load the model
+    dirname = os.getcwd()
+    filename_randomforest = os.path.join(dirname, 'models/randomforest')
+    randomforest = joblib.load(filename_randomforest)
+    # read the request data from the post request
+    response = request.get_json()
+    data = json.loads(response["data"])
+    # convert the data into a numpy array
+    arr = np.array(data)
+    # scale the data
+    scaler = StandardScaler()
+    arr = scaler.fit_transform(arr)
+    # predict the response
+    predictions = randomforest.predict(arr)
+    labels = map(checklabel, predictions.tolist())
+    labels = list(labels)
+    return json.dumps(labels)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
